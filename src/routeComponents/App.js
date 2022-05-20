e9a5ccb6949ac12a3ef82158233879cefea6caab
marco junior <marcojuniorengineer@gmail.com>
2022-05-20 19:47:48 -0300

    feat: estrutura da tela de perguntas

--- src/routeComponents/App.js
+++ src/routeComponents/App.js
@@ -4,12 +4,14 @@ import Result from "./Result";
 import StartScreen from "./StartScreen";
 import VideoScreen from "./VideoScreen";
 
+import "../assets/styles/reset.css"
+
 export default function App() {
     return (
             <BrowserRouter>
             <Routes>
                 <Route path="/" element={<StartScreen/>} > </Route>
-                <Route path="/qustions" element={<QuestionScreen/>} > </Route>
+                <Route path="/questions" element={<QuestionScreen/>} > </Route>
                 <Route path="/videos" element={<VideoScreen/>} > </Route>
                 <Route path="/result" element={<Result/>} > </Route>
             </Routes>
--- src/routeComponents/StartScreen.js
+++ src/routeComponents/StartScreen.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react"
+
 export default function StartScreen() {
     return (
         <h1>eu sou a página inicial</h1>
